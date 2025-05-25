c3d701bf138fb73501c14738a1b4055ae887a88a
Jiun Kong <bukgeuk1234@gmail.com>
2025-05-25 14:09:06 +0900

    Edit packet structure Struct-based into Interface-based

--- GodotNetCore/PacketUtils.cs
+++ GodotNetCore/PacketUtils.cs
@@ -5,14 +5,33 @@ using ENet;
 using MessagePack;
 
 namespace GodotNetCore {
-    public struct ParsedPacket {
+    [MessagePackObject]
+    public struct PacketHeader {
+        [Key(0)]
         public UInt16 packetTypeId;
-        public byte[] rawData;
+        [Key(1)]
+        public Int64 timestamp;
     }
 
+    public struct ParsedPacket {
+        public PacketHeader header;
+        public byte[] rawData;
+    }
     public static class PacketUtils {
         private readonly static Dictionary<string, UInt16> typeNameToId = new Dictionary<string, UInt16>();
         private readonly static Dictionary<UInt16, string> idToTypeName = new Dictionary<UInt16, string>();
+
+        public enum PredefinedPacketTypeId: UInt16 {
+            CreateSession = UInt16.MaxValue,
+            JoinSession = UInt16.MaxValue - 1,
+            LeaveSession = UInt16.MaxValue - 2
+        }             
+
+        static PacketUtils() {
+            RegisterPacketType((UInt16)PredefinedPacketTypeId.CreateSession, "CreateSession");
+            RegisterPacketType((UInt16)PredefinedPacketTypeId.JoinSession, "JoinSession");
+            RegisterPacketType((UInt16)PredefinedPacketTypeId.LeaveSession, "LeaveSession");
+        }
 
         public static void RegisterPacketType(UInt16 typeId, string typeName) {
             typeNameToId.Add(typeName, typeId);
@@ -29,11 +48,19 @@ namespace GodotNetCore {
             else return null;
         }
 
-        public static Packet CreatePacket<T>(UInt16 packetType, T data, PacketFlags flags = PacketFlags.None) where T : notnull {
+        public static Packet CreatePacket<T>(UInt16 packetType, T data, PacketFlags flags = PacketFlags.None, Int64? timestamp = null) where T : notnull {
             List<byte> bytes = new List<byte>();
 
-            bytes.AddRange(BitConverter.GetBytes(packetType));
-            bytes.AddRange(MessagePackSerializer.Serialize(data));
+            byte[] serializedData = MessagePackSerializer.Serialize(data);
+
+            PacketHeader header;
+            header.packetTypeId = packetType;
+            header.timestamp = timestamp ?? DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+            byte[] serializedHeader = MessagePackSerializer.Serialize(header);
+
+            bytes.AddRange(BitConverter.GetBytes(serializedHeader.Length));
+            bytes.AddRange(serializedHeader);
+            bytes.AddRange(serializedData);
 
             Packet packet = default;
             packet.Create(bytes.ToArray(), flags);
@@ -41,11 +68,11 @@ namespace GodotNetCore {
             return packet;
         }
 
-        public static Packet createPacket<T>(string packetTypeName, T data) where T : notnull {
+        public static Packet CreatePacket<T>(string packetTypeName, T data, PacketFlags flags = PacketFlags.None, Int64? timestamp = null) where T : notnull {
             if (typeNameToId.TryGetValue(packetTypeName, out var typeName)) {
-                return CreatePacket(typeName, data);
+                return CreatePacket(typeName, data, flags, timestamp);
             } else {
-                throw new KeyNotFoundException($"There is no packet type for \"{packetTypeName}\"");
+                throw new UnknownPacketTypeException(packetTypeName);
             }
         }
 
@@ -55,15 +82,16 @@ namespace GodotNetCore {
             byte[] bytes = new byte[packet.Length];
             packet.CopyTo(bytes);
 
-            ppacket.packetTypeId = BitConverter.ToUInt16(bytes, 0);
-
-            const int headerSize = sizeof(UInt16);
+            Int32 headerSize = BitConverter.ToInt32(bytes);
+            bytes = bytes.Skip(sizeof(Int32)).ToArray();
+            ppacket.header = MessagePackSerializer.Deserialize<PacketHeader>(bytes);
+            
             ppacket.rawData = bytes.Skip(headerSize).ToArray();
 
             return ppacket;
         }
 
-        public static T ParseRawData<T>(byte[] rawData) where T : notnull {
+        public static T ParseRawData<T>(PacketHeader header, byte[] rawData) where T : notnull {
             return MessagePackSerializer.Deserialize<T>(rawData);
         }
     }
---
+++ GodotNetCore/Session.cs
@@ -0,0 +1,127 @@
+﻿using ENet;
+using MessagePack;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GodotNetCore {
+    public interface ISessionCreation {
+        public string name { get; set; }
+        public string? password { get; set; }
+        public byte maxPlayers { get; set; }
+        public bool isPrivate { get; set; }
+        public string authorToken { get; set; }
+    }
+
+    public interface ISessionInfo {
+        public string name { get; }
+        public string sessionId { get; }
+        public byte maxPlayers { get;}
+        public byte currentPlayers { get; }
+        public bool isPrivate { get; }
+        public bool hasPassword { get; }
+        public string authorName { get; }
+    }
+
+    public interface ISessionList {
+        public UInt32 sessionCount { get; }
+        public ISessionInfo[] sessionInfoList { get; }
+    }
+
+    public interface ISessionJoin {
+        public string sessionId { get; set; }
+        public string password { get; set; }
+        public string userToken { get; set; }
+    }
+
+    public interface ISessionResult {
+        public bool success { get; }
+        public byte cause { get; }
+        public ISessionInfo? sessionInfo { get; }
+    }
+
+    public delegate void SessionResultEventHandler(ISessionResult data);
+
+    public class CreateSessionPacketHandler: PacketHandler<ISessionResult> {
+        protected override void Handle(ISessionResult data) {
+            SessionManager.OnSessionCreationResultHandler?.Invoke(data);
+        }
+    }
+
+    public class JoinSessionPacketHandler: PacketHandler<ISessionResult> {
+        protected override void Handle(ISessionResult data) {
+            SessionManager.OnSessionJoinResultHandler?.Invoke(data);
+        }
+    }
+
+    public static class SessionManager {
+        public static byte sessionChannel { get; set; } = 0;
+        public static PacketFlags sessionFlags { get; set; } = PacketFlags.None;
+        public static ISessionInfo? currentSession { get; private set; } = null;
+
+        public static SessionResultEventHandler? OnSessionJoinResultHandler { get; private set; } = null;
+        public static SessionResultEventHandler? OnSessionCreationResultHandler { get; private set; } = null;
+        public static SessionResultEventHandler? OnSessionLeaveResultHandler { get; private set; } = null;
+
+        public static Task<ISessionResult> CreateNewSession(ISessionCreation info) {
+            if (currentSession != null) {
+                throw new SessionExistsException(currentSession);
+            }
+
+            var tcs = new TaskCompletionSource<ISessionResult>();
+
+            OnSessionCreationResultHandler = (ISessionResult result) => {
+                tcs.SetResult(result);
+                if (result.success) {
+                    currentSession = result.sessionInfo;
+                }
+            };
+
+            Packet packet = PacketUtils.CreatePacket((UInt16)PacketUtils.PredefinedPacketTypeId.CreateSession, info, sessionFlags);
+            NetworkManager.SendPacket(sessionChannel, packet);
+
+            return tcs.Task;
+        }
+
+        public static Task<ISessionResult> JoinSession(ISessionJoin info) {
+            if (currentSession != null) {
+                throw new SessionExistsException(currentSession);
+            }
+
+            var tcs = new TaskCompletionSource<ISessionResult>();
+
+            OnSessionJoinResultHandler = (ISessionResult result) => {
+                tcs.SetResult(result);
+                if (result.success) {
+                    currentSession = result.sessionInfo;
+                }
+            };
+
+            Packet packet = PacketUtils.CreatePacket((UInt16)PacketUtils.PredefinedPacketTypeId.JoinSession, info, sessionFlags);
+            NetworkManager.SendPacket(sessionChannel, packet);
+
+            return tcs.Task;
+        }
+
+        public static Task<ISessionResult> LeaveSession() {
+            if (currentSession == null) {
+                throw new SessionNotFoundException();
+            }
+
+            var tcs = new TaskCompletionSource<ISessionResult>();
+
+            OnSessionLeaveResultHandler = (ISessionResult result) => {
+                tcs.SetResult(result);
+                if (result.success) {
+                    currentSession = null;
+                }
+            };
+
+            Packet packet = PacketUtils.CreatePacket((UInt16)PacketUtils.PredefinedPacketTypeId.LeaveSession, sessionFlags);
+            NetworkManager.SendPacket(sessionChannel, packet);
+
+            return tcs.Task;
+        }
+    }
+}
