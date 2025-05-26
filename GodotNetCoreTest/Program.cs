f39cdfec2357e9da22fb88cfa77cedbe67a443a3
Jiun Kong <bukgeuk1234@gmail.com>
2025-05-26 17:43:52 +0900

    Replace test project into console app

---
+++ GodotNetCoreTest/Program.cs
@@ -0,0 +1,6 @@
+﻿using GodotNetCore;
+
+NetworkManager.Activate();
+Console.WriteLine("NetworkManager activated.");
+bool res = await NetworkManager.Connect("127.0.0.1", 12345);
+Console.WriteLine(res);
